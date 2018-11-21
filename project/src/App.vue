9ec7685a6e310e65b309a30bc161ca8edba6c7bf
linfuhua <466177611@qq.com>
2018-11-21 12:55:42 +0800

    页脚

--- project/src/App.vue
+++ project/src/App.vue
@@ -28,11 +28,13 @@
     </div>
       <router-view/>
     </div>
+    <nav-footer></nav-footer>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import NavFooter from '@/components/module/NavFooter'
 export default {
   name: 'App',
   data(){
@@ -40,6 +42,9 @@ export default {
       activeIndex: '1',
       inputInfo:"请输入",
     }
+  },
+  components:{
+    NavFooter
   },
   computed: {
     ...mapState({
@@ -88,13 +93,17 @@ export default {
 
 <style>
 @import './css/header.scss';
+@import "./css/footer.css";
 html,body{
   margin:0px;
   padding:0px;
+  width: 100%;
+  height: 100%;
 }
-@media (min-width: 970px) { 
+
+@media (min-width: 970px) {
   .container{
-    padding:0px 120px;  
-  } 
+    padding:0px 120px;
+  }
 }
 </style>
--- project/src/components/module/NavFooter.vue
+++ project/src/components/module/NavFooter.vue
@@ -1,16 +1,36 @@
 <template>
-    <div>
-        hello word
+  <div class="footer">
+    <p class="footer-cr">
+      <router-link class="link" href="javascript:void(0)" to="/home">关于我们</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">帮助中心</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">商家入驻</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">联系我们</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">法律声明</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">消费者保障</router-link>
+      <router-link class="link" href="javascript:void(0)" to="/">廉正举报</router-link>
+    </p>
+    <p>
+      Copyright 2015-2018 pinduoduo.com 版权所有 (增值电信业务经营许可证：沪B2-20170215)
+    </p>
+    <div class="footer-record">
+      <a class="record-link" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31010502000392">
+        <span class="record-img"></span> <span class="record-num">沪公网安备 31010502000392号 </span>
+      </a>
+      <span>&nbsp;&nbsp;&nbsp;&nbsp;（沪）网械平台备字[2018]第00003号</span>
+      <a class="record-link" target="_blank" href="/assets/img/bg1136.jpg">
+        <span class="record-num">&nbsp;|&nbsp;&nbsp;营业执照</span>
+      </a>
     </div>
+  </div>
 </template>
 <style>
 </style>
 <script>
-    export default {
-        data() {
-            return {
-                msg: 'hello world'
-            }
-        }
+  export default {
+    data() {
+      return {
+        msg: 'hello world'
+      }
     }
+  }
 </script>
