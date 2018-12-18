87e4666fb90ee89a6e453f1cb3ecd1eba0191c19
wjr2020github <1911662143@qq.com>
2018-12-18 17:25:47 +0800

    客户管理

--- shopBg/src/views/Management/ClientListManage.vue
+++ shopBg/src/views/Management/ClientListManage.vue
@@ -4,10 +4,10 @@
         <span>客户名单管理&nbsp;--&nbsp;<i class="present">客户信息列表</i></span>
       </div>
       <!--工具条-->
-      <el-col :span="24" class="toolbar" style="padding-bottom: 0px;margin-top: 10px">
+      <el-col :span="24" class="toolbar" style="padding-bottom: 0px;margin-top: 10px;">
         <el-form :inline="true" :model="filters">
           <el-form-item>
-            <el-input v-model="inputValue" placeholder="请输入客户姓名/账号"></el-input>
+            <el-input v-model="filters.keyword" placeholder="请输入客户姓名/账号"></el-input>
           </el-form-item>
           <el-form-item>
             <el-button type="primary" v-on:click="getUsers">查询</el-button>
@@ -15,11 +15,20 @@
           <el-form-item>
             <el-button type="primary" @click="handleAdd">新增</el-button>
           </el-form-item>
+          <el-button size="small" class="add_coupon" @click="backCoupon" v-if="isBack==true"><img src="/static/back.png"/>客户名单列表</el-button>
         </el-form>
       </el-col>
       <!--新增界面-->
-      <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+      <el-dialog title="新增" :visible.sync="addFormVisible" :close-on-click-modal="false">
         <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+          <el-form-item label="账号" prop="account">
+            <el-input v-model="addForm.account" auto-complete="off">
+              <el-button type="primary" slot="append" icon="el-icon-search" @click.native="getClient"></el-button>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="昵称" prop="nickname">
+            <el-input v-model="addForm.nickname" auto-complete="off"></el-input>
+          </el-form-item>
           <el-form-item label="姓名" prop="name">
             <el-input v-model="addForm.name" auto-complete="off"></el-input>
           </el-form-item>
@@ -29,19 +38,13 @@
               <el-radio class="radio" :label="0">女</el-radio>
             </el-radio-group>
           </el-form-item>
-          <el-form-item label="年龄">
-            <el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
-          </el-form-item>
-          <el-form-item label="生日">
-            <el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
-          </el-form-item>
-          <el-form-item label="地址">
-            <el-input type="textarea" v-model="addForm.addr"></el-input>
+          <el-form-item label="电话">
+            <el-input v-model="addForm.phone" auto-complete="off"></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button @click.native="addFormVisible = false">取消</el-button>
-          <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+          <el-button type="primary" @click.native="addSubmit">提交</el-button>
         </div>
       </el-dialog>
       <!--内容-->
@@ -58,12 +61,12 @@
             <th class="">操作</th>
           </tr>
           </thead>
-          <tbody>
+          <tbody v-if="isNodata==false">
           <tr v-for="(item,index) of clientList" :key="index">
             <td class="" width="4%">
               <span class="client-id">{{item.clientID}}</span>
             </td>
-            <td class="" width="19%">
+            <td class="" width="19%" style="text-align: left">
               <span class="client-img"><img :src="item.clientImg"></span>
               <span class="client-account">{{item.clientAccount}}</span>
             </td>
@@ -81,8 +84,16 @@
             </td>
             <td>
               <div style="margin-bottom: 8px">
-                <el-button type="success" size="mini">查看<i class="el-icon-rank"></i></el-button>
+                <el-button type="success" size="mini" @click="handleCheck(item,index)">查看<i class="el-icon-rank"></i>
+                </el-button>
               </div>
+            </td>
+          </tr>
+          </tbody>
+          <tbody v-if="isNodata==true">
+          <tr>
+            <td colspan="7">
+              <div>暂时没有数据</div>
             </td>
           </tr>
           </tbody>
@@ -104,11 +115,34 @@
           </tfoot>
         </table>
       </div>
+      <!--查看界面-->
+      <el-dialog title="查看" :visible.sync="checkFormVisible" :close-on-click-modal="false">
+        <el-form :model="checkForm" label-width="80px" :rules="checkFormRules" ref="checkForm">
+          <el-form-item label="账号" prop="account">
+            <el-input v-model="checkForm.account" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="昵称" prop="nickname">
+            <el-input v-model="checkForm.nickname" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="checkForm.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio-group v-model="checkForm.sex">
+              <el-radio class="radio" :label="1">男</el-radio>
+              <el-radio class="radio" :label="0">女</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="电话">
+            <el-input v-model="checkForm.phone" auto-complete="off"></el-input>
+          </el-form-item>
+        </el-form>
+      </el-dialog>
     </div>
 </template>
 <script>
     export default {
-        data() {
+      data() {
             return {
               msg: '客户名单管理',
               clientList: [],
@@ -116,38 +150,48 @@
                 {
                   clientID: 1,
                   clientImg: '../../../static/headerImg2.png',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
+                  clientAccount: '12ds5678912',
+                  nickName: '轻易放飞',
+                  clientName: '火红瑞',
                   sex: '男',
-                  clientPhone: '136597*****',
+                  clientPhone: '134597*****',
                   status: 0
+                },
+                {
+                  clientID: 1,
+                  clientImg: '../../../static/touxiang.jpg',
+                  clientAccount: '7rfds5678912',
+                  nickName: '阿瓦罗萨',
+                  clientName: '火红瑞',
+                  sex: '男',
+                  clientPhone: '187557*****',
+                  status: 1
                 },
                 {
                   clientID: 2,
                   clientImg: '../../../static/headerImg.jpg',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
-                  sex: '男',
-                  clientPhone: '136597*****',
-                  status: 0
+                  clientAccount: 'all456dasdsfsd',
+                  nickName: '阿罗拉露',
+                  clientName: '卿叶子',
+                  sex: '女',
+                  clientPhone: '124597*****',
+                  status: 1
                 },
                 {
                   clientID: 3,
                   clientImg: '../../../static/touxiang.jpg',
-                  clientAccount: 'hsad45658dfsd',
+                  clientAccount: 'hsad4sda8dfsd',
                   nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
-                  sex: '男',
+                  clientName: '倾听雪',
+                  sex: '女',
                   clientPhone: '136597*****',
                   status: 0
                 },
                 {
                   clientID: 4,
                   clientImg: '../../../static/headerImg2.png',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
+                  clientAccount: 'fdd4das8dfsd',
+                  nickName: '火情风风',
                   clientName: '倾听瑞',
                   sex: '男',
                   clientPhone: '136597*****',
@@ -156,67 +200,98 @@
                 {
                   clientID: 5,
                   clientImg: '../../../static/headerImg.jpg',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
-                  sex: '男',
-                  clientPhone: '136597*****',
+                  clientAccount: 'xzd4fdf8dfsd',
+                  nickName: '菲菲伊雪',
+                  clientName: '段欢欢',
+                  sex: '女',
+                  clientPhone: '187597*****',
                   status: 0
                 },
                 {
                   clientID: 6,
                   clientImg: '../../../static/headerImg2.png',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
+                  clientAccount: 'wqad456fd8dfsd',
+                  nickName: '一听一周',
+                  clientName: '段瑞',
                   sex: '男',
-                  clientPhone: '136597*****',
+                  clientPhone: '137497*****',
                   status: 0
                 },
                 {
                   clientID: 7,
                   clientImg: '../../../static/headerImg.jpg',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
-                  sex: '男',
-                  clientPhone: '136597*****',
-                  status: 0
+                  clientAccount: '45sd5658dfsd',
+                  nickName: '红琴轻语',
+                  clientName: '琴清雨',
+                  sex: '女',
+                  clientPhone: '116578*****',
+                  status: 2
                 },
                 {
                   clientID: 8,
                   clientImg: '../../../static/headerImg2.png',
-                  clientAccount: 'hsad45658dfsd',
-                  nickName: '凮易菲菲',
-                  clientName: '倾听瑞',
+                  clientAccount: 'qew45dgsdfsd',
+                  nickName: '风清玉宇',
+                  clientName: '轻语也',
                   sex: '男',
-                  clientPhone: '136597*****',
+                  clientPhone: '136457*****',
                   status: 0
                 }
               ],
-              inputValue: '',
+              isNodata: false, // 是否有数据
+              isBack: false, // 是否返回
+              filters: {
+                keyword: ''
+              },
 
               addFormVisible: false,//新增界面是否显示
-              addLoading: false,
               addFormRules: {
-                name: [
-                  { required: true, message: '请输入姓名', trigger: 'blur' }
-                ]
+                account: [
+                  { required: true, message: '请输入账号', trigger: 'blur' }
+                ],
+//                nickname: [
+//                  { required: true, message: '请输入昵称', trigger: 'blur' }
+//                ],
+//                name: [
+//                  { required: true, message: '请输入姓名', trigger: 'blur' }
+//                ]
               },
+              getClientList: [], //新增界面查询暂存
               //新增界面数据
               addForm: {
+                account: '',
+                nickname: '',
                 name: '',
                 sex: -1,
-                age: 0,
-                birth: '',
-                addr: ''
-              }
+                phone: '',
+              },
+
+              checkFormVisible: false,//查看界面是否显示
+              checkFormRules: {
+                account: [
+                  {required: true, message: '请输入账号', trigger: 'blur'}
+                ],
+              },
+              //查看界面数据
+              checkForm: {
+                account: '',
+                nickname: '',
+                name: '',
+                sex: -1,
+                phone: '',
+              },
             }
         },
       mounted(){
-        this.clientList = this.dataList;
+        this.showDataList();
       },
       methods: {
+        // 显示数据列表
+        showDataList(){
+          this.clientList = this.dataList;
+          this.showData(this.clientList);
+//        this.addFormVisible = true;
+        },
         pageChange(val){
 //        console.log(`当前页: ${val}`);
         },
@@ -226,45 +301,139 @@
         nextClick(val){
 //        console.log(`下一页: ${val}`);
         },
+        //返回客户列表
+        backCoupon(){
+          this.$router.push('/ClientListManage')
+          this.clientList = this.dataList;
+          this.showData(this.clientList);
+          this.isBack = false;
+        },
+        // 显示数据
+        showData(item){
+          if(item.length==0){
+            this.isNodata=true;
+          }else{
+            this.isNodata=false;
+          }
+        },
         //显示新增界面
         handleAdd() {
           this.addFormVisible = true;
           this.addForm = {
+            account: '',
+            nickname: '',
             name: '',
             sex: -1,
-            age: 0,
-            birth: '',
-            addr: ''
+            phone: '',
           };
+        },
+        //性别显示转换
+        formatSex(sex) {
+          let s = sex == '男' ? 1 : sex == '女' ? 0 : -1;
+          return s;
+        },
+        //查询用户列表
+        getUsers() {
+          let para = {keyword: this.filters.keyword};
+          let urlStr = '/ClientListManage?clientKeyword=' + para.keyword;
+          if(para.keyword==""){
+            urlStr = '/ClientListManage?clientKeyword=null'
+            para.keyword = 0
+          }
+          this.$router.push(urlStr);
+          this.isBack = true;
+//          console.log('para.keyword==>'+para.keyword);
+          this.clientList = [];
+          this.dataList.forEach((item) => {
+//            console.log('item.clientAccount==>'+item.clientAccount);
+            if (item.clientAccount == para.keyword || item.clientName == para.keyword) {
+              this.clientList.push(item);
+            }
+          });
+          this.showData(this.clientList);
+        },
+        //新增界面查询用户
+        getClient(){
+          let para = {account: this.addForm.account};
+          this.getClientList = [];
+          this.addForm.nickname='';
+          this.addForm.name='';
+          this.addForm.sex=-1;
+          this.addForm.phone='';
+          this.dataList.forEach((item) => {
+            if (item.clientAccount == para.account) {
+              this.getClientList.push(item);
+            }
+          });
+          if (this.getClientList.length == 0) {
+            this.$message({
+              message: '警告，暂查无结果',
+              type: 'warning'
+            });
+          } else {
+            this.$message({
+              message: '成功，查询结果信息已经输入',
+              type: 'success'
+            });
+            this.getClientList.forEach((item) => {
+//              console.log(item.clientAccount+" "+item.nickName+" "+item.clientName+" "+item.sex+" "+item.clientPhone);
+              this.addForm.nickname=item.nickName;
+              this.addForm.name=item.clientName;
+              this.addForm.sex=this.formatSex(item.sex);
+              this.addForm.phone=item.clientPhone;
+            })
+          }
         },
         //新增
         addSubmit() {
           this.$refs.addForm.validate((valid) => {
             if (valid) {
               this.$confirm('确认提交吗？', '提示', {}).then(() => {
-                this.addLoading = true;
                 //NProgress.start();
                 let para = Object.assign({}, this.addForm);
-                para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-                addUser(para).then((res) => {
-                  this.addLoading = false;
-                  //NProgress.done();
-                  this.$message({
-                    message: '提交成功',
-                    type: 'success'
-                  });
-                  this.$refs['addForm'].resetFields();
-                  this.addFormVisible = false;
-                  this.getUsers();
+                //add success
+                this.$message({
+                  message: '提交成功,未完成功能',
+                  type: 'success'
                 });
               });
             }
           });
         },
+        //查看
+        handleCheck(item,index){
+          this.checkFormVisible = true;
+          console.log(item.nickName);
+          console.log(index);
+          this.checkForm.account=item.clientAccount;
+          this.checkForm.nickname=item.nickName;
+          this.checkForm.name=item.clientName;
+          this.checkForm.sex=this.formatSex(item.sex);
+          this.checkForm.phone=item.clientPhone;
+        },
       }
     }
 </script>
 <style>
+  /* 工具条 */
+  .toolbar .add_coupon{
+    border-radius: 5px;
+    border:1px solid #409EFF;
+    color: #409EFF;
+    float: right;
+  }
+  .toolbar .add_coupon img{
+    vertical-align: middle;
+    margin-right: 3px;
+  }
+  /* 新增对话框 */
+  .el-dialog .el-input .el-input-group__append:hover{
+    color: #fff;
+    background-color: #409eff;
+    border-color: #409eff;
+  }
+
+  /* 内容 */
   table{border:1px solid #DDD;width: 100%;border-collapse: collapse;margin-top: 25px}
   table thead{
     border-bottom: 1px solid #ddd;
--- shopBg/src/views/Management/OrderItem.vue
+++ shopBg/src/views/Management/OrderItem.vue
@@ -33,54 +33,52 @@
     </td>
     <td>
       <div style="margin-bottom: 8px">
-                <span v-if="orderList.tradingStatus==0">
-                  <el-button type="success" size="mini" @click="dialogFormVisible = true">发货<i
-                    class="el-icon-location-outline"></i>
-                  </el-button>
-                  <!-- 快递对话框 -->
-                  <el-dialog title="物流快递" :visible.sync="dialogFormVisible">
-                    <el-form :model="form">
-                      <el-form-item label="快递名称" :label-width="formLabelWidth">
-                        <el-select
-                          style="width: 100%"
-                          v-model="form.trackingName"
-                          filterable
-                          allow-create
-                          default-first-option
-                          placeholder="请输入或选择快递">
-                          <el-option
-                            v-for="item in options5"
-                            :key="item.value"
-                            :label="item.label"
-                            :value="item.value">
-                          </el-option>
-                        </el-select>
-                      </el-form-item>
-                      <el-form-item label="快运单号" :label-width="formLabelWidth">
-                        <el-input v-model="form.trackingNumber" autocomplete="off"></el-input>
-                      </el-form-item>
-                    </el-form>
-                    <div slot="footer" class="dialog-footer">
-                      <el-button @click="dialogFormVisible = false">取 消</el-button>
-                      <el-button type="primary" @click="dialogFormVisible = false">确认</el-button>
-                    </div>
-                  </el-dialog>
-                </span>
+        <span v-if="orderList.tradingStatus==0">
+          <el-button type="success" size="mini" @click="dialogFormVisible = true">发货<i
+            class="el-icon-location-outline"></i>
+          </el-button>
+          <!-- 快递对话框 -->
+          <el-dialog title="物流快递" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+              <el-form-item label="快递名称" :label-width="formLabelWidth">
+                <el-select
+                  style="width: 100%"
+                  v-model="form.trackingName"
+                  filterable
+                  allow-create
+                  default-first-option
+                  placeholder="请输入或选择快递">
+                  <el-option
+                    v-for="item in options5"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="快运单号" :label-width="formLabelWidth">
+                <el-input v-model="form.trackingNumber" autocomplete="off"></el-input>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="dialogFormVisible = false">确认</el-button>
+            </div>
+          </el-dialog>
+        </span>
         <span v-if="orderList.tradingStatus==3">
-                   <el-button type="success" size="mini">退款<i class="el-icon-remove-outline"></i>
-                   </el-button>
-                </span>
+          <el-button type="success" size="mini">退款<i class="el-icon-remove-outline"></i>
+          </el-button>
+        </span>
       </div>
       <div style="margin-bottom: 8px">
-              <span v-if="orderList.tradingStatus==0||orderList.tradingStatus==3">
-                <el-button size="mini">取消<i class="el-icon-remove-outline"></i></el-button>
-              </span>
+        <span v-if="orderList.tradingStatus==0||orderList.tradingStatus==3">
+          <el-button size="mini">取消<i class="el-icon-remove-outline"></i></el-button>
+        </span>
         <span v-if="orderList.tradingStatus==2||orderList.tradingStatus==4">
-                <el-button size="mini">删除<i class="el-icon-delete"></i></el-button>
-              </span>
-        <span v-if="orderList.tradingStatus==1">
-                已发货
-              </span>
+          <el-button size="mini">删除<i class="el-icon-delete"></i></el-button>
+        </span>
+        <span v-if="orderList.tradingStatus==1">已发货</span>
       </div>
     </td>
   </tr>
